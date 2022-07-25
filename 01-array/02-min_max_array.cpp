19f59a856354f507e5ab00f20622d45566828726
SHRUTI SINGH <shruticourses16@gmail.com>
2022-07-25 19:42:03 +0530

    commit

--- 01-array/02-min_max_array.cpp
+++ 01-array/02-min_max_array.cpp
@@ -2,17 +2,41 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+
+// using c ++ stl
+// int main()
+// {
+//     int n;
+//     cin>>n;
+//     vector<int> arr;
+//     for(int i=0;i<n;++i){
+//         int x;
+//         cin>>x;
+//         arr.push_back(x);
+//     }
+//     cout<<"Min: "<<*min_element(arr.begin(),arr.end())<<endl;
+//     cout<<"Max: "<<*max_element(arr.begin(),arr.end())<<endl;
+//     return 0;
+// }
+
+int main(){
     int n;
     cin>>n;
-    vector<int> arr;
+    int arr[n];
     for(int i=0;i<n;++i){
-        int x;
-        cin>>x;
-        arr.push_back(x);
+        cin>>arr[i];
     }
-    cout<<"Min: "<<*min_element(arr.begin(),arr.end())<<endl;
-    cout<<"Max: "<<*max_element(arr.begin(),arr.end())<<endl;
+    int minimum = INT_MAX;
+    int maximum = INT_MIN;
+    for(int i=0;i<n;++i){
+        if(minimum>=arr[i]){
+            minimum = arr[i]; 
+        }
+        if(maximum<=arr[i]){
+            maximum = arr[i]; 
+        }
+    }
+    cout<<"Min: "<<minimum<<endl;
+    cout<<"Max: "<<maximum<<endl;
     return 0;
 }
